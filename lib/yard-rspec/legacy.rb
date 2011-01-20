2d300fba6afc6580b18892562781d418a3af3d0f
kputnam <putnam.kyle@gmail.com>
2011-01-20 11:12:30 -0600

    Extended Ruby 1.8 and 1.9 handlers to recognize more specs
    -- Arbitrary nesting is supported, with contexts inside or outside the
       describe "#method" blocks. The first argument to describe must be
       a class, nested class, instance "#method" or class ".method"
    -- Ignore argument list for describe "#method(a, b = nil, &c)" etc so we
       match the right AST node
    -- Support for pending specs (specs without a body)
    -- Support for its(:property) { ... }, it { ... }, and specify { ... }.
       The spec description is guessed from the content of the block
    -- Ignores RSpec metadata like :pending => true

--- lib/yard-rspec/legacy.rb
+++ lib/yard-rspec/legacy.rb
@@ -1,31 +1,102 @@
 class LegacyRSpecDescribeHandler < YARD::Handlers::Ruby::Legacy::Base
-  MATCH = /\Adescribe\s+(.+?)\s+(do|\{)/
+  MATCH = /\Adescribe\s+(.+)\s+(?:do|\{)/
   handles MATCH
   
   def process
-    objname = statement.tokens.to_s[MATCH, 1].gsub(/["']/, '')
-    obj = {:spec => owner ? (owner[:spec] || "") : ""}
-    obj[:spec] += objname
-    parse_block :owner => obj
+    describes = statement.tokens.to_s[MATCH, 1].gsub(/["']/, "")
+
+    # Remove the argument list from describe "#method(a, b, &c)"
+    if arguments = describes[/[#.](?:.+?)(\([^)]*\))$/, 1]
+      describes = describes[0, describes.length - arguments.length]
+    end
+
+    unless owner.is_a?(Hash)
+      pwner = Hash[:describes => describes, :context => ""]
+      parse_block(:owner => pwner)
+    else
+      describes = owner[:describes] + describes
+      pwner = owner.merge(:describes => describes)
+      parse_block(:owner => pwner)
+    end
   rescue YARD::Handlers::NamespaceMissingError
   end
 end
 
-class LegacyRSpecItHandler < YARD::Handlers::Ruby::Legacy::Base
-  MATCH = /\Ait\s+['"](.+?)['"]\s+(do|\{)/
+class LegacySpecContextHandler < YARD::Handlers::Ruby::Legacy::Base
+  MATCH = /\Acontext\s+(['"])(.+)\1(?:.*?)\s+(?:do|\{)/
   handles MATCH
-  
+
   def process
-    return if owner.nil?
-    obj = P(owner[:spec])
-    return if obj.is_a?(Proxy)
-    
-    (obj[:specifications] ||= []) << {
-      :name => statement.tokens.to_s[MATCH, 1],
-      :file => parser.file,
-      :line => statement.line,
-      :source => statement.block.to_s
-    }
+    if owner.is_a?(Hash)
+      context = statement.tokens.to_s[MATCH, 2]
+      context = owner[:context] + context + " "
+
+      parse_block(:owner => owner.merge(:context => context))
+    end
   end
 end
 
+class LegacyRSpecItHandler < YARD::Handlers::Ruby::Legacy::Base
+  MATCH = /\A(?:it|specify)\s+(['"])(.+?)\1.*?(?:(\s+(?:do|\{))|$)/
+
+  handles MATCH
+  handles /\A(?:it|specify)\s+.*?(?:do|\{)/
+  
+  def process
+    return unless owner.is_a?(Hash)
+    return unless owner[:describes]
+
+    node = YARD::Registry.resolve(nil, owner[:describes], true)
+    spec = statement.tokens.to_s[MATCH, 2] || "untitled spec"
+
+    unless node
+      log.warn "Unknown node #{owner[:describes]} for spec defined in #{parser.file} near line #{statement.line}"
+      return
+    end
+
+    source = statement.block.to_s.strip
+
+    (node[:specifications] ||= []) << \
+      Hash[ :name => owner[:context] + spec,
+            :file => parser.file,
+            :line => statement.line,
+            :source => source ]
+  end
+end
+
+class LegacyRSpecItsHandler < YARD::Handlers::Ruby::Legacy::Base
+# MATCH = /\Aits\s*(\(?)\s*[:]?(['"]?)(.+)\2\1(?:(\s+(?:do|\{))|$)/
+# handles MATCH
+
+  MATCH = /\Aits\s*([(]?)\s*:?(['"]?)([^'")]+)\2/
+  handles MATCH
+
+  def process
+    return unless owner.is_a?(Hash)
+    return unless owner[:describes]
+
+    node = YARD::Registry.resolve(nil, owner[:describes], true)
+
+    property = statement.tokens.to_s[MATCH, 3]
+    should   = statement.block.to_s[/should(?:_not)?\s+(.+)/, 1]
+
+    spec = if property and should
+             "#{property} #{should}"
+           else
+             "untitled spec"
+           end
+
+    unless node
+      log.warn "Unknown node #{owner[:describes]} for spec defined in #{parser.file} near line #{statement.line}"
+      return
+    end
+
+    source = statement.block.to_s.strip
+
+    (node[:specifications] ||= []) << \
+      Hash[ :name => owner[:context] + spec,
+            :file => parser.file,
+            :line => statement.line,
+            :source => source ]
+  end
+end
