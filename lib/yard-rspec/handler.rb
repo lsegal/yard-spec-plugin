ab90d3e59f2064b3f9862479f4de978b61630dbd
Jesús Burgos Maciá <jburmac@gmail.com>
2019-08-06 11:06:48 -0400

    Ignore extra layers of test contexts
    Two changes:
    
    - Handle calls to 'context' just the same way as 'describe'.
    - Allow additional intermediate blocks.
    
    Closes #6.

--- lib/yard-rspec/handler.rb
+++ lib/yard-rspec/handler.rb
@@ -1,14 +1,18 @@
 class RSpecDescribeHandler < YARD::Handlers::Ruby::Base
   handles method_call(:describe)
-  
+  handles method_call(:context)
+
   def process
     objname = statement.parameters.first.jump(:string_content).source
+
     if statement.parameters[1]
       src = statement.parameters[1].jump(:string_content).source
       objname += (src[0] == "#" ? "" : "::") + src
     end
     obj = {:spec => owner ? (owner[:spec] || "") : ""}
-    obj[:spec] += objname
+    if statement.parameters.first.jump(:var_ref).type == :var_ref || objname.start_with?("#")
+      obj[:spec] += objname
+    end
     parse_block(statement.last.last, owner: obj)
   rescue YARD::Handlers::NamespaceMissingError
   end
@@ -16,12 +20,12 @@ end
 
 class RSpecItHandler < YARD::Handlers::Ruby::Base
   handles method_call(:it)
-  
+
   def process
     return if owner.nil?
     obj = P(owner[:spec])
     return if obj.is_a?(Proxy)
-    
+
     (obj[:specifications] ||= []) << {
       name: statement.parameters.first.jump(:string_content).source,
       file: statement.file,
